a8debf0586e37f421d57d457ae1c3250ad8db298
LevaniJangveladze <chumala12@gmail.com>
2026-06-01 21:14:16 +0400

    Add second task of C#

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Enter the first number:");
+           int a = int.Parse(Console.ReadLine());
+           
+           Console.Write("Enter the second number: ");
+           int b = int.Parse(Console.ReadLine());
+           
+           Console.WriteLine($"{a} + {b} = {a + b}");
+           Console.WriteLine($"{a} - {b} = {a - b}");
+           Console.WriteLine($"{a} * {b} = {a * b}");
+           
+           Console.WriteLine($"{a} / {b} = {(double)a / b}");
+           
+           Console.WriteLine($"{a} % {b} = {a % b}");
+        }
+    }
+}
