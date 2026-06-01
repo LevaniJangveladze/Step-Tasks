b0a60fed122e9f66b4484ac84dbd1f9013349414
LevaniJangveladze <chumala12@gmail.com>
2026-06-01 20:53:19 +0400

    Add C# types practice task

---
+++ FirstC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Luka";
+
+        int age = 25;
+        byte favoriteNumber = 7;
+        long bigNumber = 9000000000;
+
+        double height = 1.82;
+        decimal salary = 2500.50m;
+
+        bool isStudent = true;
+        char grade = 'A';
+
+        int[] examScores = { 85, 90, 78, 100 };
+        
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}, Favorite Number: {favoriteNumber}");
+        Console.WriteLine($"Big Number: {bigNumber}");
+        Console.WriteLine($"Height: {height}m");
+        Console.WriteLine($"Salary: {salary}GEL");
+        Console.WriteLine($"Is a student? {isStudent}");
+    }
+}
